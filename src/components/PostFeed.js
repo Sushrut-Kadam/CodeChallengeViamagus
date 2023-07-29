4537650c00719c68258d614ba048884d50a9ba85
Sushrut <sushrutkadam.123@gmail.com>
2023-07-29 15:36:33 +0530

    Pagination for viewing posts added

--- src/components/PostFeed.js
+++ src/components/PostFeed.js
@@ -1,17 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import "../css/postfeed.css"
 import {Post} from "./Post";
 import { Link } from "react-router-dom";
+import ReactPaginate from "react-paginate";
 
 const PostFeed = ({posts}) => {
+    const [pageNumber, setPageNumber] = useState(0);
+
+    const postsPerPage = 10;
+    const pagesVisited = pageNumber*postsPerPage;
+    const pageCount = Math.ceil(posts.length/postsPerPage);
+
+    const displayPosts = posts
+    .slice(pagesVisited, pagesVisited + postsPerPage)
+    .map(post => (
+        <div className="PostWrapper" key={post.id}> 
+            <Post post={post}/>
+            <Link to={`post/${post.id}`}>View Post</Link>
+        </div>
+    ))
+
+    const handlePageChange = ({selected}) => {
+        setPageNumber(selected);
+    }
+
     return(
         <div>
-            {posts.map(post => (
-                <div className="PostWrapper" key={post.id}> 
-                    <Post post={post}/>
-                    <Link to={`post/${post.id}`}>View Post</Link>
-                </div>
-            ))}
+            {displayPosts}
+            <ReactPaginate
+                previousLabel="prev"
+                nextLabel="next"
+                pageCount={pageCount}
+                onPageChange={handlePageChange}
+                containerClassName="paginationButtons"
+                activeClassName="paginationActive"
+            />
         </div>
     );
 }
